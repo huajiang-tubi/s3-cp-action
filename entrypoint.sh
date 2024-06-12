b187ab91f39b2de2d213327f2c7c3d74c55584a1
Hua Jiang <huajiang@tubi.tv>
2024-06-12 21:48:30 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,6 +17,8 @@ ${INPUT_AWS_REGION}
 text
 EOF
 
+ls -d ~
+
 # Sync using our dedicated profile and suppress verbose messages.
 # All other flags are optional via the `args:` directive.
 sh -c "aws s3 cp ${INPUT_SOURCE} ${INPUT_DEST} \
